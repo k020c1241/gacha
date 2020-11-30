7405b64bad558e2abefc44b87a02268b80fd1a77
Kensho Sugishita <k020c1241@it-neec.jp>
2020-11-30 16:47:08 +0000

    prototype

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request
 import random
+import numpy as np
+cards = ["N", "N+", "R", "R+", "SR", "SR+"]
+count_list = [0, 0, 0, 0, 0, 0]
+SRp_count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 app = Flask(__name__)
+
 #トップページ
 @app.route("/")
 def top():
     title = "ガチャシミュレーター"
-    return render_template('gacha.html', 
-                           title = title)
+    return render_template(
+      'gacha.html',
+      title=title)
+
 #ガチャ後
 @app.route('/post', methods=['POST', 'GET'])
 def gacha():
@@ -17,106 +24,142 @@ def gacha():
         if "rare" in request.form:
             title = "レアガチャを回しました"
             result = RareGacha()
-            rare = RareChoice()
             vo.price = vo.price + 100
-            vo.count = vo.count + 1
-            vo.card[rare[0]] = vo.card[rare[0]] + 1
+            vo.Tcount = vo.Tcount + 1
+            vo.Scount = vo.Scount + 1
+            msg = SRpComp()
         if 'rare11' in request.form:
             title = "11連ガチャを回しました！"
             result = Rare11Gacha()
-            rare = Rare11Choice()
             vo.price = vo.price + 1000
-            vo.count = vo.count + 1
-            for i in range(0,11):
-                vo.card[rare[0]] = vo.card[rare[0]] + 1
+            vo.Tcount = vo.Tcount + 1
+            vo.Ccount = vo.Ccount + 1
+            msg = SRpComp()
         if 'reset' in request.form:
             title = "リセットしました"
             vo.price = 0
-            vo.count = 0
-            vo.card = {"N":0, "N+":0, "R":0, "R+":0, "SR":0, "SR+":0} 
+            vo.Tcount = 0
+            vo.Scount = 0
+            vo.Ccount = 0
             result = ""
-    return render_template('gacha.html',
-                            result = result,
-                            rare = rare,
-                            title = title,
-                            vo = vo,
-                            count = vo.count,
-                            price = vo.price,
-                            card = vo.card)
+            msg = SRpComp()
+        """"
+        if 'aa' in request.form:
+            title = "debug"
+            result = debugfunc()
+            vo.price = vo.price + 1000
+            vo.count = vo.count + 1
+            msg = SRpComp()
+          """
+    return render_template(
+        'gacha.html',
+        result=result,
+        rare=rare,
+        title=title,
+        vo=vo,
+        Scount=vo.Scount,
+        Ccount=vo.Ccount,
+        Tcount=vo.Tcount,
+        price=vo.price,
+        msg = msg,
+        debug = SRp_count,
+        N = count_list[0],
+        Np = count_list[1],
+        R = count_list[2],
+        Rp = count_list[3],
+        SR = count_list[4],
+        SRp = count_list[5])
 
 
 #レアガチャ
 def RareGacha():
     image_list = []
-    pick_rare = RareChoice()
+    pick_rare = np.random.choice(cards, p=[0.33, 0.25, 0.20, 0.15, 0.05, 0.02])
+    RareCount(pick_rare, count_list)
     image_list.append(ImageChoice(pick_rare))
     return image_list
-def RareChoice():
-    cards = ["N","N+","R","R+","SR","SR+"]
-    return random.choices(cards,weights = [33, 25, 20, 15, 5, 2])
+
 #11連ガチャ
 def Rare11Gacha():
     image_list = []
     for i in range(0,10):
-       PickRare = Rare11Choice()
-       image_list.append(ImageChoice(PickRare))
-    last = ["SR"]
-    image_list.append(ImageChoice(last))
+      pick_rare = np.random.choice(cards, p=[0.0, 0.0, 0.57, 0.30, 0.10, 0.03])
+      RareCount(pick_rare, count_list)
+      image_list.append(ImageChoice(pick_rare))
+    image_list.append(ImageChoice("SR"))
+    RareCount("SR", count_list)
+    return image_list   
+""""
+#debug:SR+コンプ
+def debugfunc():
+    image_list = []
+    for i in range(25):
+      RareCount("SR+", count_list)
+      image_list.append(ImageChoice("SR+"))
     return image_list 
-def Rare11Choice():
-    cards = ["N","N+","R","R+","SR","SR+"]
-    return random.choices(cards,weights = [0, 0, 57, 30, 10, 3])
+"""
 
 #画像を指定してhtmlへ
 def ImageChoice(pick_rare):
-   image_num = PickNum(pick_rare)
-   image_path = "static/images/" + pick_rare[0] + "/" + pick_rare[0] + str(image_num[0]) + ".jpg"
-   return image_path
+    image_num = PickNum(pick_rare)
+    image_path = "static/images/" + pick_rare + "/" + pick_rare + str(image_num) + ".jpg"
+    if pick_rare == "SR+":
+      SRp_count[image_num] += 1
+    return image_path
+
 #画像番号の範囲
+rare_num = {"N":42, "N+":44, "R":113, "R+":98, "SR":24, "SR+":25}
 def PickNum(pick_rare):
-    list = []
-    rare_num = 0
-    if pick_rare[0] == "N":
-       rare_num = 42
-    elif pick_rare[0] == "N+":
-       rare_num = 44
-    elif pick_rare[0] == "R":
-       rare_num = 113  
-    elif pick_rare[0] == "R+":
-       rare_num = 98
-    elif pick_rare[0] == "SR":
-       rare_num = 24 
-    elif pick_rare[0] == "SR+":
-       rare_num = 25
-    list.append(random.randint(0,rare_num))
-    return list
+    num = np.random.randint(0,rare_num.get(pick_rare))
+    return num
+
+#累計
+def RareCount(pick_rare, count_list):
+  list = ["N", "N+", "R", "R+", "SR", "SR+"]
+  num = list.index(pick_rare)
+  count_list[num] += 1
+#SRカウント
+def SRpComp():
+  msg = ""
+  if 0 in SRp_count:
+    pass
+  else:
+    msg ="SR+を全て獲得しました！！"
+  return msg
 
 #ガチャ回数、課金額の記録
 class VO(object):
     def __init__(self):
-        self._count = 0 # 回数
-        self._price = 0 # 課金額
-        self._card = {"N":0, "N+":0, "R":0, "R+":0, "SR":0, "SR+":0} #排出カード
-    
-    def getcount(self):
-        return self._count
-    def setcount(self, count):
-        self._count = count
+        self._Scount = 0 # 単発回数
+        self._Ccount = 0 # 連続回数
+        self._Tcount = 0 #合計回数
+        self._price = 0  # 課金額
 
-    def getprice(self):
+    def get_Scount(self):
+        return self._Scount
+    def set_Scount(self, Scount):
+        self._Scount = Scount
+
+    def get_Ccount(self):
+        return self._Ccount
+    def set_Ccount(self, Ccount):
+        self._Ccount = Ccount
+
+    def get_Tcount(self):
+        return self._Tcount
+    def set_Tcount(self, Tcount):
+        self._Tcount = Tcount
+
+    def get_price(self):
         return self._price
-    def setprice(self, price):
+    def set_price(self, price):
         self._price = price
-    
-    def getcard(self):
-        return self._card
-    def setcard(self, card):
-        self._card = card
 
-    count = property(getcount, setcount)
-    price = property(getprice, setprice)
-    card = property(getcard, setcard)
+    Scount = property(get_Scount, set_Scount)
+    Ccount = property(get_Ccount, set_Ccount)
+    Tcount = property(get_Tcount, set_Tcount)
+    price = property(get_price, set_price)
 
 vo = VO()
 
-app.run(host="0.0.0.0",debug=True)
+app.run(host="0.0.0.0", debug=True)
--- templates/gacha.py
+++ templates/gacha.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import random
+import numpy as np
 
 app = Flask(__name__)
 
@@ -20,23 +21,17 @@ def gacha():
         if "rare" in request.form:
             title = "レアガチャを回しました"
             result = RareGacha()
-            rare = RareChoice()
             vo.price = vo.price + 100
             vo.count = vo.count + 1
-            vo.card[rare[0]] = vo.card[rare[0]] + 1
         if 'rare11' in request.form:
             title = "11連ガチャを回しました！"
             result = Rare11Gacha()
-            rare = Rare11Choice()
             vo.price = vo.price + 1000
             vo.count = vo.count + 1
-            for i in range(0, 11):
-                vo.card[rare[0]] = vo.card[rare[0]] + 1
         if 'reset' in request.form:
             title = "リセットしました"
             vo.price = 0
             vo.count = 0
-            vo.card = {"N": 0, "N+": 0, "R": 0, "R+": 0, "SR": 0, "SR+": 0}
             result = ""
     return render_template(
         'gacha.html',
@@ -46,58 +41,51 @@ def gacha():
         vo=vo,
         count=vo.count,
         price=vo.price,
-        card=vo.card)
+        N = count_list[0],
+        N+ = count_list[1],
+        R = count_list[2],
+        R+ = count_list[3],
+        SR = count_list[4],
+        SR+ = count_list[5])
 
 
 #レアガチャ
-def RareChoice():
-    cards = ["N", "N+", "R", "R+", "SR", "SR+"]
-    return random.choices(cards, weights=[33, 25, 20, 15, 5, 2])
 def RareGacha():
     image_list = []
-    pick_rare = RareChoice()
+    pick_rare = np.random.choice(cards, p=[0.33, 0.25, 0.20, 0.15, 0.05, 0.02])
+    RareCount(pick_rare, count_list)
     image_list.append(ImageChoice(pick_rare))
     return image_list
 
-
 #11連ガチャ
-def Rare11Choice():
-    cards = ["N", "N+", "R", "R+", "SR", "SR+"]
-    return random.choices(cards, weights=[0, 0, 57, 30, 10, 3])
 def Rare11Gacha():
     image_list = []
-    for i in range(0, 10):
-        PickRare = Rare11Choice()
-        image_list.append(ImageChoice(PickRare))
-    last = ["SR"]
-    image_list.append(ImageChoice(last))
-    return image_list
-
+    for i in range(0,10):
+      pick_rare = np.random.choice(cards, p=[0.0, 0.0, 0.57, 0.30, 0.10, 0.03])
+      RareCount(pick_rare, count_list)
+      image_list.append(ImageChoice(pick_rare))
+    image_list.append(ImageChoice("SR"))
+    RareCount("SR", count_list)
+    return image_list   
 
 
 #画像を指定してhtmlへ
 def ImageChoice(pick_rare):
     image_num = PickNum(pick_rare)
-    image_path = "static/images/" + pick_rare[0] + "/" + pick_rare[0] + str(image_num[0]) + ".jpg"
+    image_path = "static/images/" + pick_rare + "/" + pick_rare + str(image_num) + ".jpg"
     return image_path
+
 #画像番号の範囲
+rare_num = {"N":42, "N+":44, "R":113, "R+":98, "SR":24, "SR+":25}
 def PickNum(pick_rare):
-    list = []
-    rare_num = 0
-    if pick_rare[0] == "N":
-        rare_num = 42
-    elif pick_rare[0] == "N+":
-        rare_num = 44
-    elif pick_rare[0] == "R":
-        rare_num = 113
-    elif pick_rare[0] == "R+":
-        rare_num = 98
-    elif pick_rare[0] == "SR":
-        rare_num = 24
-    elif pick_rare[0] == "SR+":
-        rare_num = 25
-    list.append(random.randint(0, rare_num))
-    return list
+    num = np.random.randint(0,rare_num.get(pick_rare))
+    return num
+
+#累計
+def RareCount(pick_rare, count_list):
+  list = ["N", "N+", "R", "R+", "SR", "SR+"]
+  num = list.index(pick_rare)
+  count_list[num] += 1
 
 
 #ガチャ回数、課金額の記録
@@ -105,29 +93,20 @@ class VO(object):
     def __init__(self):
         self._count = 0  # 回数
         self._price = 0  # 課金額
-        self._card = {"N": 0,"N+": 0,"R": 0,"R+": 0,"SR": 0,"SR+": 0}  #排出カード
 
     def getcount(self):
         return self._count
-
     def setcount(self, count):
         self._count = count
 
     def getprice(self):
         return self._price
-
     def setprice(self, price):
         self._price = price
 
-    def getcard(self):
-        return self._card
-
-    def setcard(self, card):
-        self._card = card
-
     count = property(getcount, setcount)
     price = property(getprice, setprice)
-    card = property(getcard, setcard)
+
 vo = VO()
 
 app.run(host="0.0.0.0", debug=True)
